3057bdd3a8aebe5495193abe1082643cf1f30532
minh tran <minhduytranct2017@gmail.com>
2024-05-11 01:53:49 +0700

    feat: config responsive

--- src/components/InfoCard/index.jsx
+++ src/components/InfoCard/index.jsx
@@ -1,10 +1,14 @@
 const InfoCard = ({ title, description, image }) => {
   return (
-    <div className='bg-white rounded-[30px] w-[680px] h-[640px] flex flex-col'>
-      <img src={image} alt='Demo' className='w-full rounded-t-[30px]' />
-      <div className='pl-10 pt-8 bg-white h-full rounded-b-[30px]'>
-        <h2 className='font-bold text-[33.6px] leading-[47.04px] mb-5'>{title}</h2>
-        <p className='w-[460px] text-xl font-normal leading-7 tracking-[0.02em]'>{description}</p>
+    <div className='bg-white rounded md:rounded-[30px] w-[280px] sm:w-[400px] lg:w-[480px] xl:w-[600px] xl:h-[580px] 3xl:w-[680px] 3xl:h-[640px] flex flex-col'>
+      <img src={image} alt='Demo' className='w-full rounded-t md:rounded-t-[30px]' />
+      <div className='px-5 py-4 md:px-7 md:py-6 lg:px-8 lg:py-7 xl:px-9 xl:py-8 3xl:pl-10 3xl:pt-9 bg-white h-full rounded-b md:rounded-b-[30px]'>
+        <h2 className='font-bold text-xl md:text-2xl xl:text-3xl 2xl:text-[33.6px] leading-[47.04px] mb-2 md:mb-5'>
+          {title}
+        </h2>
+        <p className='xl:w-[420px] 3xl:w-[460px] text-base lg:text-xl font-normal leading-7 tracking-[0.02em] text-[#171717]'>
+          {description}
+        </p>
       </div>
     </div>
   );
--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,5 +1,6 @@
 import DemoImg from '../../assets/images/demoImg.png';
 import InfoCard from '../../components/InfoCard';
+import { useRef, useEffect, useState } from 'react';
 
 const InfoCardContent = [
   {
@@ -23,19 +24,53 @@ const InfoCardContent = [
 ];
 
 const Homepage = () => {
+  const scrollRef = useRef();
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth < 720) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+  });
+
+  useEffect(() => {
+    const el = scrollRef.current;
+    if (el && !isMobile) {
+      const onWheel = (e) => {
+        if (e.deltaY === 0) return;
+        e.preventDefault();
+        el.scrollTo({
+          left: el.scrollLeft + e.deltaY * 4,
+          behavior: 'smooth',
+        });
+      };
+      el.addEventListener('wheel', onWheel);
+      return () => el.removeEventListener('wheel', onWheel);
+    }
+  }, [isMobile]);
+
   return (
-    <div className='pt-[177px] pb-[157px] bg-[#F2F2F2]'>
-      <div className='w-full flex flex-col items-center space-y-[57px]'>
-        <h1 className='w-fit font-normal text-[77px] text-center leading-[92.4px]'>
+    <div className='pt-[60px] pb-10 sm:pt-[80px] min-h-[100vh] sm:pb-[60px] md:pt-[100px] md:pb-[80px] lg:pt-[120px] lg:pb-[100px] xl:pt-[140px] xl:pb-[120px] 3xl:pt-[177px] 3xl:pb-[157px] bg-[#F2F2F2]'>
+      <div className='w-full flex flex-col items-center space-y-10 md:space-y-[48px] xl:space-y-[52px] 2xl:space-y-[57px]'>
+        <h1 className='w-fit font-normal text-2xl sm:text-[32px] md:text-3xl lg:text-[44px] xl:text-[60px] 3xl:text-[77px] text-center leading-8 sm:leading-[40px] md:leading-[48px] lg:leading-[60px] xl:leading-[80px] 2xl:leading-[92.4px]'>
           Why people love to
           <br /> work with us
         </h1>
-        <button className='border-[1px] border-[#666666] rounded-[50px] py-5 px-10 font-bold text-base leading-[22.4px]'>
+        <button className='border-[1px] border-[#666666] rounded-[50px] py-2 px-4 md:py-3 md:px-6 xl:py-4 xl:px-8 3xl:py-5 3xl:px-10 font-bold text-base leading-[22.4px]'>
           Read more
         </button>
       </div>
-      <div className='overflow-x-auto'>
-        <div className='px-[260px] mt-[100px] flex space-x-10 w-fit'>
+      <div
+        ref={scrollRef}
+        className='md:overflow-x-auto hide-scrollbar mt-[60px] md:mt-[80px] 2xl:mt-[100px]'
+      >
+        <div className='md:px-[100px] lg:px-[140px] xl:px-[180px] 2xl:px-[220px] 3xl:px-[260px] flex flex-col mx-auto md:mx-0 md:flex-row space-y-5 md:space-y-0 md:space-x-10 w-fit'>
           {InfoCardContent.map((content, index) => (
             <InfoCard
               key={index}
